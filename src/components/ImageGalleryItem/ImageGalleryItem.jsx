dfe29ebc7781182b59d9919d944f4f07a6ed53cc
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-05-02 14:48:15 +0300

    refactoring into hooks ImageGelleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,27 +1,21 @@
 import { Modal } from 'components/Modal/Modal';
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { GalleryImage } from './ImageGalleryItem.styled';
 import PropTypes from 'prop-types';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    shownModal: false,
-  };
-  onModal = () => {
-    this.setState(({ shownModal }) => ({ shownModal: !shownModal }));
-  };
-  render() {
-    const { item } = this.props;
-    const { webformatURL } = item;
-    return (
-      <li>
-        <GalleryImage onClick={this.onModal} src={webformatURL} alt="img" />
-        {this.state.shownModal && <Modal onClose={this.onModal} image={item} />}
-      </li>
-    );
-  }
-}
+export const ImageGalleryItem = ({ item }) => {
+  const [shownModal, setShowModal] = useState(false);
 
+  const onModal = () => {
+    setShowModal(!shownModal);
+  };
+  return (
+    <li>
+      <GalleryImage onClick={onModal} src={item.webformatURL} alt="img" />
+      {shownModal && <Modal onClose={onModal} image={item} />}
+    </li>
+  );
+};
 ImageGalleryItem.propTypes = {
   item: PropTypes.object.isRequired,
 };
