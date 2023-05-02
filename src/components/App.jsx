35af009d864a980e176eeac22df8bb2812d17610
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-05-02 21:57:04 +0300

    fixed error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,14 +14,14 @@ export const App = () => {
   const [totalHits, setTotalHits] = useState(0);
   const [page, setPage] = useState(1);
 
-  const handleSubmit = async inputData => {
-    if (inputData.trim() === '') {
+  const handleSubmit = async searchTerm => {
+    if (searchTerm.trim() === '') {
       Notiflix.Notify.info('You cannot search by empty field, try again.'); //Перевірка чи рядок не пустий
       return;
     }
     setStatus('pending');
     try {
-      const { totalHits, hits } = await fetchImages(inputData, page);
+      const { totalHits, hits } = await fetchImages(searchTerm, 1);
       if (hits.length < 1) {
         setStatus('idle');
         Notiflix.Notify.failure(
@@ -29,8 +29,9 @@ export const App = () => {
         );
       } else {
         setItems(hits);
-        setInputData(inputData);
+        setInputData(searchTerm);
         setTotalHits(totalHits);
+        setPage(1);
         setStatus('resolved');
       }
     } catch (error) {
@@ -38,26 +39,6 @@ export const App = () => {
     }
   };
 
-  //   setStatus('pending');
-  //   setPage(1);
-
-  //   try {
-  //     const { totalHits, hits } = await fetchImages(inputData, 1);
-  //     if (hits.length < 1) {
-  //       setStatus('idle');
-  //       Notiflix.Notify.failure(
-  //         'Sorry, there are no images matching your search query. Please try again.'
-  //       );
-  //     } else {
-  //       setItems(hits);
-  //       setInputData(inputData);
-  //       setTotalHits(totalHits);
-  //       setStatus('resolved');
-  //     }
-  //   } catch (error) {
-  //     setStatus('rejected');
-  //   }
-  // };
   const onNextPage = async () => {
     setStatus('pending');
     try {
@@ -69,45 +50,46 @@ export const App = () => {
       setStatus('rejected');
     }
   };
+
+  let content;
+  switch (status) {
+    case 'idle':
+      content = <Searchbar onSubmit={handleSubmit} />;
+      break;
+    case 'pending':
+      content = (
+        <>
+          <GlobalStyle />
+          <Searchbar onSubmit={handleSubmit} />
+          <ImageGallery page={page} items={items} />
+          <Loader />
+          {totalHits > 12 && <Button onClick={onNextPage} />}
+        </>
+      );
+      break;
+    case 'rejected':
+      content = (
+        <>
+          <Searchbar onSubmit={handleSubmit} />
+          <p>Something wrong, try later</p>
+        </>
+      );
+      break;
+    case 'resolved':
+      content = (
+        <>
+          <GlobalStyle />
+          <Searchbar onSubmit={handleSubmit} />
+          <ImageGallery page={page} items={items} />
+          {totalHits > 12 && totalHits > items.length && (
+            <Button onClick={onNextPage} />
+          )}
+        </>
+      );
+      break;
+    default:
+      content = null;
+  }
+
+  return content;
 };
-
-let content;
-switch (status) {
-  case 'idle':
-    content = <Searchbar onSubmit={handleSubmit} />;
-    break;
-  case 'pending':
-    content = (
-      <>
-        <GlobalStyle />
-        <Searchbar onSubmit={handleSubmit} />
-        <ImageGallery page={page} items={items} />
-        <Loader />
-        {totalHits > 12 && <Button onClick={onNextPage} />}
-      </>
-    );
-
-    break;
-  case 'rejected':
-    content = (
-      <>
-        <Searchbar onSubmit={handleSubmit} />
-        <p>Something wrong, try later</p>
-      </>
-    );
-    break;
-  case 'resolved':
-    content = (
-      <>
-        <GlobalStyle />
-        <Searchbar onSubmit={handleSubmit} />
-        <ImageGallery page={page} items={items} />
-        {totalHits > 12 && totalHits > items.length && (
-          <Button onClick={onNextPage} />
-        )}
-      </>
-    );
-    break;
-  default:
-    content = null;
-}
